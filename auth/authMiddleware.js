cbc0699a1224477088f51d60de61fb2c1b444cdc
evensky1 <fromthedeepsize@gmail.com>
2023-02-10 20:54:43 +0300

    authMiddleware.js was refactored

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = function (req, res, next) {
-    if (!req.headers.authorization) return res.status(401).json({message: 'User is not authenticated'})
-    const token = req.headers.authorization.split(' ')[1]
-    if (!token) return res.status(401).json({message: 'User is not authenticated'})
-    req.user = jwt.verify(token, process.env.SECRET_KEY)
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        req.user = jwt.verify(token, process.env.SECRET_KEY)
+    } catch (e) {
+        return res.status(401).json({message: 'User is not authenticated'})
+    }
     next()
 }
